5677aaaeafd4124c98da13bce75a16873f36e36c
rashedrk <rashedrk001@gmail.com>
2023-05-16 21:07:17 +0600

    Delete a coffee

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,37 @@
+import Swal from "sweetalert2";
 
 
 const CoffeeCard = ({ coffee }) => {
-    const { name, chef, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, chef, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffee/${_id}`,{
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your coffee has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <figure><img src={photo} alt="Movie" /></figure>
@@ -18,7 +48,7 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="btn-group btn-group-vertical space-y-4">
                         <button className="btn ">View</button>
                         <button className="btn">Edit</button>
-                        <button className="btn">X</button>
+                        <button onClick={() => handleDelete(_id)} className="btn bg-orange-500 border-none hover:bg-orange-400">X</button>
                     </div>
                 </div>
             </div>
